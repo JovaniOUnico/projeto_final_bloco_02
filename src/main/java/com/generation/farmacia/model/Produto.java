e1781fcfcfaa6f6cf3e5a1562106dcfa7b1b72a6
JovaniOUnico <jovani.souza.almeida@gmail.com>
2025-05-28 13:30:53 -0300

    :tada: adicionando crud de produtos relação bidirecional

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,28 +1,31 @@
 package com.generation.farmacia.model;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+//import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name = "tb_categorias")
-public class Categoria {
+@Table(name = "tb_produto")
+public class Produto {
 
 	@Id // Primary Key
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
@@ -30,30 +33,32 @@ public class Categoria {
 		
 	@Column(length = 100)
 	@NotBlank(message = "O atributo nome é obrigatório!")
-	@Size(max = 100, message = "O atributo nome deve ter no minimo 5 e no máximo 100 caracteres.")
+	@Size(min = 5, max = 100, message = "O atributo nome deve ter no minimo 5 e no máximo 100 caracteres.")
 	@Pattern(regexp = "^[^0-9].*", message = "O nome não pode ser apenas numérico")
 	private String nome;
 	
 	@Column(length = 1000)
 	@NotBlank(message = "O atributo descricao é obrigatório!")
-	@Size(max = 1000, message = "O atributo descricao deve ter no minimo 10 e no máximo 1000 caracteres.")
+	@Size(min = 10, max = 1000, message = "O atributo descricao deve ter no minimo 10 e no máximo 1000 caracteres.")
 	@Pattern(regexp = "^[^0-9].*", message = "O descricao não pode ser apenas numérico")
 	private String descricao;
+	
+	@NotNull(message = "O preço é obrigatório.") // Garante que não seja nulo
+	@DecimalMin(value = "0.01", message = "O preço deve ser positivo e maior que zero.") // Garante que seja um valor válido para preço
+	private BigDecimal preco;
+
+	//TODO A Imagem deve ser um conjunto de recursos de midia para o usuario saber mais sobre o produto
+	@Column(length = 1000)
+	@Pattern(regexp = "^[^0-9].*", message = "A imagem não pode ser apenas numérico")
+	private String img;
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("categoria")
-	private List<Produto> produto;
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
 	
-	public List<Produto> getProduto() {
-		return produto;
-	}
-	public void setPostagem(List<Produto> produto) {
-		this.produto = produto;
-	}
-
 	public Long getId() {
 		return id;
 	}
@@ -77,6 +82,22 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+	
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
 
 	public LocalDateTime getData() {
 		return data;
@@ -86,4 +107,12 @@ public class Categoria {
 		this.data = data;
 	}
 
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setTema(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
 }
